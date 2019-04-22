2408787a70136247525652d255d09f3478aab8d3
agundlapallimxns <44643976+agundlapallimxns@users.noreply.github.com>
2019-04-22 08:58:15 -0500

    ChemWell Parser Updated Code
    Chem Well Parser Updated and formatted

--- scripts/Development/Compile/Chemwell/ChemwellParser.py
+++ scripts/Development/Compile/Chemwell/ChemwellParser.py
@@ -12,6 +12,7 @@ import pandas as pd
 import os
 import time
 import csv
+import sqlalchemy as sql
 
 # Default Parameters setting
 path='//mxns.loc//shares//NA-Instruments//Prod//Crete//Import//Chemwell//RawFiles//'
@@ -21,6 +22,26 @@ importDest='//mxns.loc//shares//NA-Instruments//Prod//Crete//impf//'
 colsOrder=['Sample.Sample_Number', 'Test.Analysis','Result.Name', 'Result.Entry', 'Result.Entered_By']
 lab='CRE'
 inst='Chemwell'
+dbHost='mxnunx41'
+dbPort='1521'
+dbUser='lims'
+dbPassword='Mumwit63'
+dbName='hwi1'
+dburl='oracle://'
+
+# get Data from DB
+def getDataFromDB(sampleNumbersList):
+    dburl = 'oracle://'+dbUser+':'+dbPassword+'@'+dbHost+':'+dbPort+'/'+dbName
+    engine=sql.create_engine(dburl)
+    resultdf=pd.DataFrame()
+    sampleNumbrStr=(', '.join("'" + item + "'" for item in sampleNumbersList))
+    queryText="SELECT tsttbl.SAMPLE_NUMBER as Sample, tsttbl.TEST_NUMBER, max(tsttbl.REPLICATE_COUNT) AS REPLICATE_COUNT FROM ( SELECT SAMPLE_NUMBER , TEST_NUMBER, REPLICATE_COUNT FROM TEST"
+    queryText=queryText+" WHERE SAMPLE_NUMBER IN ("+sampleNumbrStr+")AND ANALYSIS = 'ALLERGLIAD' ) tsttbl GROUP BY (tsttbl.SAMPLE_NUMBER, tsttbl.TEST_NUMBER)"
+    conn = engine.connect()
+    resultdf = pd.read_sql(queryText, engine)
+    conn.close()
+    resultdf=resultdf.drop(['replicate_count'], axis=1)
+    return resultdf
 
 # Get the List of files
 files = os.listdir(pathrawfiles)
@@ -35,17 +56,23 @@ for name in files:
 	dataxl111['Sample'] = dataxl111['Sample'].str[2:] # format the sample numbers
 	dataxl111 = pd.melt(dataxl111, id_vars=['Sample'], value_vars=['Abs', 'Conc']) # Pivot the samples numbers
 	# formatting the dataframe to get in respect to the Import Functionality
-	dataxl111 = dataxl111.replace('Abs', 'Absorbance')
-	dataxl111 = dataxl111.replace('Conc', 'Gliadin (Component of Gluten)')
-	dataxl111 = dataxl111.rename(index=str, columns={'Sample': 'Sample.Sample_Number', 'variable': 'Result.Name',
-													 'value': 'Result.Entry'})
-	dataxl111['Test.Analysis'] = 'ALLERGLIAD'
-	dataxl111['Result.Entered_By'] = 'IMPORTER'
-	dataxl111 = dataxl111.sort_values(by=['Sample.Sample_Number'])
-	colsOrder = ['Sample.Sample_Number', 'Test.Analysis', 'Result.Name', 'Result.Entry', 'Result.Entered_By']
-	dataxl111 = dataxl111.astype({"Sample.Sample_Number": int})
+	sampleNumbersList = list(dataxl111['Sample'].unique())
+	testNumberdf = getDataFromDB(sampleNumbersList)
+	testNumberdf = testNumberdf.rename(index=str, columns={'sample': 'Sample'})
+	dataxl111 = dataxl111.astype({"Sample": int})
+	dfResult = pd.merge(dataxl111, testNumberdf, on='Sample')
+	dfResult = dfResult.replace('Abs', 'Absorbance')
+	dfResult = dfResult.replace('Conc', 'Gliadin (Component of Gluten)')
+	dfResult = dfResult.rename(index=str, columns={'Sample': 'Sample.Sample_Number', 'variable': 'Result.Name',
+												   'value': 'Result.Entry', 'test_number': 'Test.Test_Number'})
+	# dfResult['Test.Analysis'] = 'ALLERGLIAD'
+	dfResult['Result.Entered_By'] = 'IMPORTER'
+	dfResult = dfResult.sort_values(by=['Sample.Sample_Number'])
+	dfResult = dfResult.astype({"Sample.Sample_Number": int})
 	timestr = time.strftime("%Y%m%d%H%M%S")
-	FileName = importDest + lab+'_'+inst+'_' + timestr + '.csv'
-	dataxl111.to_csv(FileName, index=False, encoding="utf-8",quotechar='"',quoting=csv.QUOTE_NONNUMERIC) # write  out the file to be imported
-	destination = archivedDestination + name[:-4] + '_raw.ARK' # Backup the processed file
+	FileName = importDest + lab + '_' + inst + '_' + timestr + '.csv'
+	dfResult.to_csv(FileName, index=False, encoding="utf-8", quotechar='"',
+					quoting=csv.QUOTE_NONNUMERIC)  # write  out the file to be imported
+	destination = archivedDestination + name[:-4] + '_raw.ARK'  # Backup the processed file
 	os.rename(datapathxl, destination)
+	time.sleep(2)
